5a0e60e60f554f260c3fda3b0a1be627595a7df7
Allen Wang <aw578@cornell.edu>
2025-02-26 12:04:43 -0500

    add dominator discovery

---
+++ cs6120/lesson_5/dominators.py
@@ -0,0 +1,91 @@
+import sys
+import json
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from lesson_2.blocks import function_blocks
+from lesson_2.cfg import build_cfg, change_labels
+
+def slow_traverse(cfg):
+  """build a dominance tree naively by checking all paths"""
+  dominates = {}
+  start_set = set(cfg.keys())
+  for node in cfg:
+    dominates[node] = start_set
+  dominates["entry"] = set(["entry"])
+  path = []
+  # record current path, for each node 
+  def traverse(node):
+    path.append(node)
+    new_dominates = set(path)
+    dominates[node] = new_dominates.intersection(dominates[node])
+    for succ in cfg[node]:
+      if(succ not in path):
+        # if you loop to previous node in path, it was already visited so its dominators will always be a subset of current pah
+        traverse(succ)
+    path.pop()
+  traverse("entry")
+  return dominates
+
+def invert(cfg):
+  preds = {}
+  for node in cfg:
+    preds[node] = []
+  for node in cfg:
+    for succ in cfg[node]:
+      preds[succ].append(node)
+  return preds
+
+def postorder(cfg):
+  order = []
+  path = []
+  def po(node):
+    path.append(node)
+    for succ in cfg[node]:
+      if succ not in path:
+        po(succ)
+    order.append(node)
+    path.pop()
+  po("entry")
+  return order
+
+def fast_traverse(cfg):
+  """build a dominance tree using reverse postorder?"""
+  preds = invert(cfg)
+  dominates = {}
+  start_set = set(cfg.keys())
+  for node in cfg:
+    dominates[node] = start_set
+  order = postorder(cfg)[::-1]
+
+  # iterate
+  changed = True
+  while changed:
+    changed = False
+    for node in order:
+      new_dominates = set()
+      for pred in preds[node]:
+        if(new_dominates == set()):
+          new_dominates = set(dominates[pred])
+        else:
+          new_dominates = new_dominates.intersection(dominates[pred])
+      new_dominates.add(node)
+      if(dominates[node] != new_dominates):
+        assert(len(dominates[node]) > len(new_dominates))
+        changed = True
+        dominates[node] = new_dominates
+  return dominates
+
+def compare(dom1, dom2):
+  assert(len(dom1) == len(dom2) and key in dom2 for key in dom1)
+  for key in dom1:
+    assert(dom1[key] == dom2[key])
+if __name__ == "__main__":
+  program_str = "".join(sys.stdin.readlines())
+  program = json.loads(program_str)
+  instrs = program["functions"][0]["instrs"]
+  cfg = build_cfg(change_labels(function_blocks(instrs)))
+  fast = fast_traverse(cfg)
+  print(fast)
+  slow = slow_traverse(cfg)
+  compare(fast, slow)
