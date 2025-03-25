69bcb6329ee09c925a64b6cbd88221a1437a0267
Allen Wang <aw578@cornell.edu>
2025-03-25 03:00:44 -0400

    finish a8

--- cs6120/lesson_8/test_licm.sh
+++ cs6120/lesson_8/test_licm.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+total_dyn_inst1=0
+total_dyn_inst2=0
+
 for f in benchmarks/core/*.bril; do
   echo "$f"
   args=""
@@ -15,12 +18,23 @@ for f in benchmarks/core/*.bril; do
     fi
   done < "$f"
 
-  # licm_output=$(bril2json < "$f" | python cs6120/lesson_8/licm.py | bril2txt)
-  # echo "$licm_output"
-   # output comparison
+  # Output comparison
   output1=$(bril2json < "$f" | brili -p $args 2>/dev/null)    
   output2=$(bril2json < "$f" | python cs6120/lesson_8/licm.py | brili -p $args 2>/dev/null)  
   if [ "$output1" != "$output2" ]; then
       echo "FAIL (out) $f $output1 $output2"
   fi
+
+  
+  total_inst1=$(bril2json < "$f" | brili -p $args 2>&1 > /dev/null | grep -oP '(?<=total_dyn_inst: )\d+')    
+  total_inst2=$(bril2json < "$f" | python cs6120/lesson_8/licm.py | brili -p $args 2>&1 > /dev/null | grep -oP '(?<=total_dyn_inst: )\d+')     
+  if [ "$total_inst1" -lt "$total_inst2" ]; then
+      echo "FAIL (ins) $f $total_inst1 $total_inst2"
+  fi
+  total_dyn_inst1=$((total_dyn_inst1 + total_inst1))
+  total_dyn_inst2=$((total_dyn_inst2 + total_inst2))
 done
+
+# Print the totals
+echo "Total dynamic instructions for original: $total_dyn_inst1"
+echo "Total dynamic instructions for reduced: $total_dyn_inst2"
